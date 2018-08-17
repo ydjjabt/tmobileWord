afd53c0a027f0b445cceecedf21c1a06727c7911
john chau <ydjjabt@gmail.com>
2018-08-16 19:00:54 -0700

    done with unit tests

--- jTest.go
+++ jTest.go
@@ -1,6 +1,8 @@
 package main
 
 import(
+"sync"
+"sort"
 "encoding/json"
 "io/ioutil"
 "os"
@@ -24,42 +26,73 @@ func openFile(fileName string) map[string]interface{}{
 
 
 func validWords(startWord string, endWord string) []string{
-
+var wg sync.WaitGroup
 dictionary := openFile("english.json")
 
-resultArray := []string{};
+// switch startWord and endWord if endWord is alphabetical order is smaller than startWord
+if endWord < startWord{
+	swap := startWord
+	startWord = endWord
+	endWord = swap
+}
 
-
+resultMap := map[string]interface{}{}
 formatStartWord := strings.ToUpper(string(startWord[0])) + startWord[1:] 
 _,exist := dictionary[formatStartWord]
 if( exist ){
-	resultArray = append(resultArray, startWord);
+	resultMap[startWord] = nil;
 }
-	
-	possibleWord := startWord
-	for i:= 1; i < len(endWord); i++{
-		possibleWord = strings.Replace(possibleWord, startWord[i:i+1], endWord[i:i+1], 1)
-		
-		formatPossibleWord  :=  strings.ToUpper(string(possibleWord[0])) + possibleWord[1:] 
-		_,exist := dictionary[formatPossibleWord]
-		if(exist){
-			resultArray = append(resultArray, possibleWord);
-		}
-	}
 
 formatEndWord := strings.ToUpper(string(endWord[0])) + endWord[1:] 
 _,existEndWord := dictionary[formatEndWord]
 if( existEndWord ){
-	resultArray = append(resultArray, endWord);
+	resultMap[endWord] = nil;
 }
+	
+	possibleWord := startWord
+	startWordLength := len(strings.Split(startWord, "") ) //using len([]rune(startWord)) doesnt give correct 'end result'. same thing with utf8.runecountsomething
+	endWordLength := len( strings.Split(endWord, "") )//therefore i split string to array and it work on equal length and unequal length of two strings.
+	var loopEnd int;
+		if startWordLength >= endWordLength{ 
+			loopEnd = endWordLength
+		}else{ 
+			loopEnd = startWordLength
+		}
+		ch := make( chan string, loopEnd )
+		for i:= 1; i < loopEnd; i++{
+			possibleWord = strings.Replace(possibleWord, startWord[i:i+1], endWord[i:i+1], 1)
+			ch <- possibleWord
+			wg.Add(1)
+			go func(ch chan string){
+				defer wg.Done()
+				pWord := <- ch
+				formatPossibleWord  :=  strings.ToUpper(string(pWord[0])) + pWord[1:] 
+				_,exist := dictionary[formatPossibleWord]
+				if(exist){
+					resultMap[pWord] = nil;
+				}
+			}(ch)
+		}
+	
+// when main is run, this validWords function is in main as part of main goroutine
+// then just put the waitGroup.Wait() here for all those goroutine returning valid words
+wg.Wait()
+
+//put only keys of map into array to get unique valid words only. no duplicate
+resultArray := []string{}
+for k,_ := range resultMap{
+ 	resultArray = append(resultArray, k)
+ }
+// sort it from a to z alphabetical order
+sort.Slice(resultArray, func(i, j int) bool {
+    return resultArray[i] < resultArray[j]
+})
 
 return resultArray;
 
 }
 
 func main(){
-
-	fmt.Println( validWords("bat", "cow"))
-	
+	fmt.Println( validWords("cat", "hopeful"))
 }
 
--- word_test.go
+++ word_test.go
@@ -13,12 +13,71 @@ if len(dictionary) == 0{
 	t.Error("dictionary is empty")
 }
 }
+//checking file type in Go
+// check if os.Open() work
 
 
-func Test_validWords(t *testing.T){
+func Test_validWords_1(t *testing.T){
 	resultArr := validWords("bat", "cow")
 	expectedArr := []string{"bat", "bot", "bow", "cow"}
 	if !reflect.DeepEqual(resultArr, expectedArr){
 		t.Error("answers is invalid")
+		t.Error("your answer:", resultArr)
+		t.Error("expected answer:", expectedArr)
+	}
+}
+
+
+func Test_validWords_2(t *testing.T){
+	resultArr := validWords("cat", "dog")
+	expectedArr := []string{"cat", "cog", "cot", "dog"}
+	if !reflect.DeepEqual(resultArr, expectedArr){
+		t.Error("answers is invalid")
+		t.Error("your answer:", resultArr)
+		t.Error("expected answer:", expectedArr)
+	}
+}
+
+
+func Test_validWords_SwitchOrder(t *testing.T){
+	resultArr := validWords("dog", "cat")
+	expectedArr := []string{"cat", "cog", "cot", "dog"}
+	if !reflect.DeepEqual(resultArr, expectedArr){
+		t.Error("answers is invalid")
+		t.Error("your answer:", resultArr)
+		t.Error("expected answer:", expectedArr)
+	}
+}
+
+
+func Test_validWords_UnequalStrings(t *testing.T){
+	resultArr := validWords("battle", "dog")
+	expectedArr := []string{"battle", "bottle", "dog"}
+	if !reflect.DeepEqual(resultArr, expectedArr){
+		t.Error("answers is invalid. ")
+		t.Error("your answer:", resultArr)
+		t.Error("expected answer:", expectedArr)
+	}
+}
+
+
+func Test_validWords_UnequalStrings_2(t *testing.T){
+	resultArr := validWords("dog", "horse")
+	expectedArr := []string{"dog", "dor", "horse"}
+	if !reflect.DeepEqual(resultArr, expectedArr){
+		t.Error("answers is invalid")
+		t.Error("your answer:", resultArr)
+		t.Error("expected answer:", expectedArr)
+	}
+}
+
+
+func Test_validWords_UnequalStrings_more(t *testing.T){
+	resultArr := validWords("cat", "hopeful")
+	expectedArr := []string{"cat", "cop", "cot", "hopeful"}
+	if !reflect.DeepEqual(resultArr, expectedArr){
+		t.Error("answers is invalid")
+		t.Error("your answer:", resultArr)
+		t.Error("expected answer:", expectedArr)
 	}
 }
